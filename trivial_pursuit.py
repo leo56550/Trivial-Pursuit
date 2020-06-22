947e24960e0a4acf761ccfcde96278b0a550b377
Leo Bultel <leo.bultel@free.fr>
2020-06-23 00:24:00 +0200

    Presque fini, reste a proposer les répoonses mélangées au hasard et à traiter le cas des couleurs des intersections.

--- trivial_pursuit.py
+++ trivial_pursuit.py
@@ -14,27 +14,32 @@ yellow_questions = [{
 "rc" : "Yellow",
 "ri_lst" : ["black", "red", "green"],
 }]
+
 orange_questions = [{
 "q" : "What city is the capital of Japan",
 "rc" : "Tokyo",
 "ri_lst" : ["Hiroshima", "Kyoto",
 "Hosaka"],
 }]
-purple_question = [{
+
+purple_questions = [{
 "q" : "What is the title of the most famous song of Prince ?",
 "rc" : "Purple rain",
 "ri_lst" : ["deep purple", "thriller","let it be"],
 }]
+
 red_questions = [{
 "q" : "What is the toughest communist country ?",
 "rc" : "North Corea",
 "ri_lst" : ["Russia", "Cuba","Poland"],
 }]
+
 green_questions = [{
 "q" : "What color is alien blood ?",
 "rc" : "Green",
 "ri_lst" : ["Red", "Blue","Brown"],
 }]
+
 blue_questions = [{
 "q" : "Of what color is Depardieu's nose ?",
 "rc" : "red ",
@@ -46,22 +51,15 @@ blue_questions = [{
 
 
 
-
-
-Base_questions = [
-{"couleur" : "yellow" , "Questions" : yellow_questions} ,
-{"couleur" : "red" , "Questions" : red_questions} ,    
-{"couleur" : "orange" , "Questions" : orange_questions} ,
-{"couleur" : "green" , "Questions" : green_questions} ,
-{"couleur" : "blue" , "Questions" : blue_questions} ,
-{"couleur" : "purple" , "Questions" : purple_questions} 
-]
-
-
-
-
-
-
+base_questions = { 
+ "yellow" :  yellow_questions ,
+ "red" :     red_questions  ,    
+ "orange" :  orange_questions ,
+ "green" :   green_questions ,
+ "blue" :    blue_questions ,
+ "purple" :  purple_questions 
+ 
+ } 
 
 
 
@@ -110,9 +108,32 @@ plateau = [
     {"type" :"normal", "couleur" : "orange" }
      ]
 
-print (plateau)
-
-
+#Cette fonction pose une question de la couleur donnée au joueur courant, s'il répond correctement, la fonction
+#retourne True sinon False
+def poserQuestion(color) : 
+    
+    if color == "white":
+        color = "red" # TODO ask the teacher what to do in this case
+    
+    questions = base_questions[color]
+    index_question = randint(1,len(questions))
+    question = questions[index_question-1]
+    print(question["q"] )
+    
+    # TODO randomize the choices proposal order
+    
+    print(" Here are 4 possibilities : "+ 
+          question["rc"] +  "(1), " + 
+          question["ri_lst"][0] + "(2) , "+
+          question["ri_lst"][1] + "(3) , "+
+          question["ri_lst"][2]) + "(4)" 
+    reponse = raw_input("1/2/3/4 ?").lstrip()
+    
+    if reponse == "1" :
+        print("You are right")
+        return True
+    
+    return False
 #Cette fonction permet de lancer un de a 6 faces et de retourner une valeur au hasard
 
 def random_de() :
@@ -148,16 +169,16 @@ def deroulement_d1_coup (joueur) :
 
     case = plateau[position_courante]
     color = case["couleur"]
-    print("It's a " + color )
+    print("It's a " + color + " question."  )
     
     #Si le joueur tombe sur une case blanche
-    if color == "white" and case["type"] ==  "normal"
+    if color == "white" and case["type"] ==  "normal" : 
         return 1 
     # Pose une question pour la couleur donnée.  
     ret = poserQuestion(color)
     
     if ret == True : 
-        if case["type"] == "intersection"
+        if case["type"] == "intersection" : 
             joueur["score"] += 1 
         return 1 
 
